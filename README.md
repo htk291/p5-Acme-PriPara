4e566d17bc46cbb3a75f35ac2109d948c5c75f37
htk291 <htk0129@gmail.com>
2014-10-07 18:21:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::PriPara - It's new $module
+
+# SYNOPSIS
+
+    use Acme::PriPara;
+
+# DESCRIPTION
+
+Acme::PriPara is ...
+
+# LICENSE
+
+Copyright (C) htk291.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+htk291 <htk0129@gmail.com>
---
+++ lib/Acme/PriPara.pm
@@ -0,0 +1,39 @@
+package Acme::PriPara;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::PriPara - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::PriPara;
+
+=head1 DESCRIPTION
+
+Acme::PriPara is ...
+
+=head1 LICENSE
+
+Copyright (C) htk291.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+htk291 E<lt>htk0129@gmail.comE<gt>
+
+=cut
+
